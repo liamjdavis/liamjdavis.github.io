a1d495775b45e5b11d2f9a41d2c266034b1ab71f
Liam Davis <davisliam123@gmail.com>
2024-03-27 21:15:14 -0400

    used inline styles to make the site responsive

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,13 +12,17 @@ const Home = () => {
     <div>
       <Navbar />
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img src={Headshot} alt="Headshot" style={{
-          width: '250px',
-          height: '250px',
-          borderRadius: '50%',
-          objectFit: 'cover',
-          marginBottom: '20px'
-        }} />
+        <img
+          src={Headshot}
+          alt="Headshot"
+          style={{
+            width: '250px',
+            height: '250px',
+            borderRadius: '50%',
+            objectFit: 'cover',
+            marginBottom: '20px',
+          }}
+        />
       </div>
 
       <div>
@@ -26,11 +30,17 @@ const Home = () => {
       </div>
 
       <div className="btn-container">
-        <button className="btn" onClick={() => window.location.href = "https://www.linkedin.com/in/liam-j-davis/"}>
+        <button
+          className="btn"
+          onClick={() => (window.location.href = "https://www.linkedin.com/in/liam-j-davis/")}
+        >
           Linkedin
         </button>
 
-        <button className="btn" onClick={() => window.location.href = "https://github.com/liamjdavis"}>
+        <button
+          className="btn"
+          onClick={() => (window.location.href = "https://github.com/liamjdavis")}
+        >
           Github
         </button>
       </div>
@@ -38,37 +48,37 @@ const Home = () => {
       <div>
         <p>
           I'm a Mathematics and Computer Science student at Amherst College passionate about the intersection
-          between software development and quantitative research. I am also interested in sports analytics 
-          (particularly baseball, football and soccer) and classical music. A fun fact about me is that I as a trombone player, I 
-          placed third at the 2022 Concours International Music Competition
-          and was a finalist at the 2023 Southeast Trombone Symposium Competition. 
-          I am constantly seeking opportunities to apply myself to new challenges. 
+          between software development and quantitative research. I am also interested in sports analytics
+          (particularly baseball, football and soccer) and classical music. A fun fact about me is that I as a trombone player, I placed third at the 2022 Concours International Music Competition
+          and was a finalist at the 2023 Southeast Trombone Symposium Competition. I am constantly seeking opportunities to apply myself to new challenges.
           Let's connect and explore opportunities to collaborate and create solutions together.
         </p>
       </div>
-      
-      <div class="container">
-        <div class="column left-column">
+
+      <div style={{ display: 'flex' }}>
+        <div className="column left-column" style={{ flex: 1 }}>
           <div>
             <h1>Education</h1>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <img src={AmherstSeal} alt="AmherstSeal" style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-                marginBottom: '20px'
-              }} />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img
+                src={AmherstSeal}
+                alt="AmherstSeal"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                  marginBottom: '20px',
+                }}
+              />
             </div>
 
             <div>
-              <h3>Amherst College 
-                <br></br>
-                <p><i>Bachelors of Arts in Mathematics and Computer Science</i>
-                  <br></br> 
+              <h3>
+                Amherst College <br />
+                <p>
+                  <i>Bachelors of Arts in Mathematics and Computer Science</i>
+                  <br />
                   August 2023 - September 2027
                 </p>
               </h3>
@@ -77,24 +87,27 @@ const Home = () => {
 
           <div>
             <h1>Industry Training Programs</h1>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <img src={imc} alt="imc" style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-                marginBottom: '20px'
-              }} />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img
+                src={imc}
+                alt="imc"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                  marginBottom: '20px',
+                }}
+              />
             </div>
 
             <div>
-              <h3>IMC Prosperity Trading Challenge
-                <br></br>
-                <p><i>Competitor</i>
-                  <br></br> 
+              <h3>
+                IMC Prosperity Trading Challenge
+                <br />
+                <p>
+                  <i>Competitor</i>
+                  <br />
                   April 2024
                 </p>
               </h3>
@@ -104,8 +117,8 @@ const Home = () => {
           <div>
             <h1>Skills</h1>
             <div>
-              <div>
-                <h3 style={{marginBottom:0}}>Programming Languages</h3>
+              <div style={{ marginBottom: 0 }}>
+                <h3>Programming Languages</h3>
                 <ul>
                   <li>Python</li>
                   <li>Java</li>
@@ -114,11 +127,11 @@ const Home = () => {
                   <li>Javascript (Node, React)</li>
                   <li>R</li>
                 </ul>
-                <br></br>
+                <br />
               </div>
-              
-              <div>
-                <h3 style={{marginBottom:0}}>Tools</h3>
+
+              <div style={{ marginBottom: 0 }}>
+                <h3>Tools</h3>
                 <ul>
                   <li>Scikit-learn</li>
                   <li>Pandas + Numpy</li>
@@ -126,11 +139,11 @@ const Home = () => {
                   <li>Tensorflow + Keras</li>
                   <li>Matplotlib + Seaborn</li>
                 </ul>
-                <br></br>
+                <br />
               </div>
 
               <div>
-                <h3 style={{marginBottom:0}}>Technologies</h3>
+                <h3>Technologies</h3>
                 <ul>
                   <li>Git + Github</li>
                   <li>Jupyter</li>
@@ -143,99 +156,84 @@ const Home = () => {
             </div>
           </div>
         </div>
-        
 
-        <div class="column right-column">
+        <div className="column right-column" style={{ flex: 1 }}>
           <div>
             <h1>Work Experience</h1>
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}>
-              <img src={hp} alt="hp" style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-                marginBottom: '20px'
-              }} />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img
+                src={hp}
+                alt="hp"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                  marginBottom: '20px',
+                }}
+              />
             </div>
 
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <h3>HP Tech Ventures
-                <br></br>
-                <p><i>Incoming Business and VC Analytics Extern</i>
-                  <br></br> 
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <h3>
+                HP Tech Ventures
+                <br />
+                <p>
+                  <i>Incoming Business and VC Analytics Extern</i>
+                  <br />
                   May 2024 - July 2024
                 </p>
               </h3>
             </div>
 
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}>
-              <img src={mammo_bsb} alt="mammo_bsb" style={{
-                width: '100px',
-                height: '100px',
-                borderRadius: '50%',
-                objectFit: 'cover',
-                marginBottom: '20px'
-              }} />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img
+                src={mammo_bsb}
+                alt="mammo_bsb"
+                style={{ width: '100px', height: '100px', borderRadius: '50%', objectFit: 'cover', marginBottom: '20px' }}
+              />
             </div>
 
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <h3>Amherst College Varsity Baseball
-                <br></br>
-                <p><i>Data and Scouting Analyst</i>
-                  <br></br> 
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <h3>
+                Amherst College Varsity Baseball
+                <br />
+                <p>
+                  <i>Data and Scouting Analyst</i>
+                  <br />
                   February 2024 - Present
 
-                  <br></br> <br></br>
-                  Collecting and analyzing data on player performance
-                  and opposing teams.
+                  <br />
+                  <br />
+                  Collecting and analyzing data on player performance and opposing teams.
                 </p>
               </h3>
             </div>
 
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}>
-              <img src={frio} alt="frio" style={{
-                width: '100px',
-                height: '100px'
-              }} />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <img src={frio} alt="frio" style={{ width: '100px', height: '100px' }} />
             </div>
 
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-              <h3>Frio Energy Partners
-                <br></br>
-                <p><i>Software Engineering Intern</i>
-                  <br></br> 
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <h3>
+                Frio Energy Partners
+                <br />
+                <p>
+                  <i>Software Engineering Intern</i>
+                  <br />
                   January 2024 - February 2024
 
-                  <br></br> <br></br>
-                  Built data pipeline from financial statements
-                  to company database.
+                  <br />
+                  <br />
+                  Built data pipeline from financial statements to company database.
                 </p>
               </h3>
             </div>
-
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
