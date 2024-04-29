abfce9ffe8d1caae232f2a029713f88b4e92b79f
liamjdavis <liam@liamsdell>
2024-04-29 14:35:58 -0400

    Updates

---
+++ precache-manifest.56fc8bcfddae68b9dbb73502a21a45bb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0c40ef01d912a32f25ad6417b07ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90787c8f517da97d17af",
+    "url": "/static/css/main.f473d6c1.chunk.css"
+  },
+  {
+    "revision": "555bb5e56d4c65b4c556",
+    "url": "/static/js/2.fb18542c.chunk.js"
+  },
+  {
+    "revision": "90787c8f517da97d17af",
+    "url": "/static/js/main.da6c2569.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4e68843091722feeebc270c4d9f9b87",
+    "url": "/static/media/Frio.e4e68843.jpg"
+  },
+  {
+    "revision": "ff01e9671d7cee4ab85283bce1d54a59",
+    "url": "/static/media/amherst baseball.ff01e967.jpg"
+  },
+  {
+    "revision": "7fe30c55f24a888b7546f9a5caf9a686",
+    "url": "/static/media/amherst seal.7fe30c55.png"
+  },
+  {
+    "revision": "a22d57b5bb905f016b95135566910761",
+    "url": "/static/media/code path.a22d57b5.jpg"
+  },
+  {
+    "revision": "ca92a539d55503e3c10372807560d8c7",
+    "url": "/static/media/cubist systematic.ca92a539.png"
+  },
+  {
+    "revision": "c6c0147f7e48a931eae499078e8efd2c",
+    "url": "/static/media/goldman sachs.c6c0147f.png"
+  },
+  {
+    "revision": "d7773dee31aec1999e80b6ea62dc4205",
+    "url": "/static/media/headshot.d7773dee.jpg"
+  },
+  {
+    "revision": "86e21cd1977e43645fe8db2523b2c10e",
+    "url": "/static/media/hp.86e21cd1.png"
+  },
+  {
+    "revision": "392fe272ab11ce569beb6cca12b85f87",
+    "url": "/static/media/imc.392fe272.jpg"
+  }
+]);
